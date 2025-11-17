a9e1e241c70f3301b76342c9387e03900effd52c
ivesh <iamvenkatesh14@gmail.com>
2025-11-17 11:12:59 +0530

    Data ignored in git ignore

--- template.py
+++ template.py
@@ -29,7 +29,7 @@ list_of_files = [
     f"{project_name}/pipeline/training_pipeline.py",
     f"{project_name}/utils/__init__.py",
     f"{project_name}/utils/main_utils.py",
-    "template/index.html",
+    "research/trial.ipynb",
     ".dockerignore",
     "app.py",
     "Dockerfile",
